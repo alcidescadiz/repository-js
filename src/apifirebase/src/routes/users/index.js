e17d4fb1b149b216d15a17d794b3f4f8c92f7ab2
alcidescadiz <alcides.cadiz@gmail.com>
2022-06-18 13:06:20 -0400

    Validaciones && auth

--- src/apifirebase/src/routes/users/index.js
+++ src/apifirebase/src/routes/users/index.js
@@ -9,15 +9,17 @@ import {collection as Collection,
 import validate from './validaciones.js';
 let nameCollection = "usuarios"
 let Usuarios = Collection(db, nameCollection)
+
+import validarUsuario from '../../auth/index.js'
 // Rutas
-routerUser.get('/api', async (req, res)=>{
+routerUser.get('/api',validarUsuario, async (req, res)=>{
     const querySnapshot = await getDocs(Usuarios);
     const datos = querySnapshot.docs.map((doc) => 
         ({ id: doc.id, ...doc.data()})
     )
     res.send({datos, total: datos.length})
 })
-routerUser.get('/api/mayoredad', async (req, res)=>{
+routerUser.get('/api/mayoredad', validarUsuario, async (req, res)=>{
     const {id} = req.params
     const querySnapshot = await getDocs(query(Usuarios, where("edad", ">", 18)))
     const datos = querySnapshot.docs.map((doc) => 
@@ -26,7 +28,7 @@ routerUser.get('/api/mayoredad', async (req, res)=>{
     res.send({datos, total: datos.length})
 })
 
-routerUser.get('/api/:id', async (req, res)=>{
+routerUser.get('/api/:id',validarUsuario, async (req, res)=>{
     const {id} = req.params
     const dato = (await getDoc(doc(db, nameCollection, id))).data()
     res.send({id, ...dato})
@@ -34,45 +36,66 @@ routerUser.get('/api/:id', async (req, res)=>{
 
 
 routerUser.post('/api', async (req, res)=>{
-    const {nombre, edad, email} = req.body
+    const {username, nombre, edad, email, password} = req.body
     const emailUnique =  (await getDocs(query(Usuarios, where("email", "==", email)))).docs
     if (emailUnique.length > 0) {
         res.send({msg: "Email ya registrado, ingresar uno valido"})
         return 
     }
-
-    if(validate(nombre, edad, email).validation){
-        const querySnapshot = await  addDoc(Usuarios, {
-            nombre: nombre, 
-            edad: edad, 
-            email: email
-        });
-        res.send({nombre, edad, email,
-            msg: "Usuario agregado con éxito"
-        })
+    let validar = validate(username, nombre, edad, email, password)
+    const USUARIO = {
+        username: username.trim(),
+        nombre: nombre.trim().toUpperCase(), 
+        edad: edad, 
+        email: email.trim().toLowerCase(),
+        password: password.trim()
+    }
+    if(validar.validation){
+        const querySnapshot = await  addDoc(Usuarios, USUARIO );
+        res.send({...USUARIO,msg: "Usuario agregado con éxito"})
     }else{
-        res.send(validate(nombre, edad, email).msg)
+        res.send(validar.msg)
     }
 })
 
+routerUser.post('/api/login', async (req, res)=>{
+    const {username, password, email} = req.body
+    const getUsuario =  (await getDocs(query(Usuarios, where("email", "==", email))))
+    const datosUsuario = getUsuario.docs.map((doc) => 
+        ({ id: doc.id, ...doc.data()})
+    )
+    let existUsername = datosUsuario[0].username === username ? true: false
+    let existPassword = datosUsuario[0].password === password ? true: false
+    let existEmail = datosUsuario[0].email === email ? true: false
 
-routerUser.put('/api/:id', async (req, res)=>{
+    if (existUsername && existPassword && existEmail) {
+        res.send(datosUsuario)
+    } else {
+        res.send({msg: 'Datos no registrados'})
+    }
+
+})
+
+routerUser.put('/api/:id',validarUsuario, async (req, res)=>{
     const {id} = req.params
-    const {nombre, email, edad} = req.body
-    if(validate(nombre, edad, email).validation){
+    const {usernameNew, nombreNew, emailNew, edadNew, passwordNew} = req.body
+    const validar = validate(usernameNew, nombreNew, edadNew, emailNew, passwordNew)
+    if(validar.validation){
         await updateDoc(doc(db, nameCollection, id),
             {
-                nombre : nombre,
-                email: email,
-                edad: edad
+                username: usernameNew.trim(),
+                nombre : nombreNew.trim().toUpperCase(),
+                email: emailNew.trim().toLowerCase(),
+                edad: edadNew,
+                password: passwordNew.trim()
             });
         res.send({message:`Documento: ${id} editado`})
     }else{
-        res.send(validate(nombre, edad, email).msg)
+        res.send(validar.msg)
     }
 })
 
-routerUser.delete('/api/:id', async (req, res)=>{
+routerUser.delete('/api/:id',validarUsuario, async (req, res)=>{
     const {id} = req.params
     await deleteDoc(doc(db, nameCollection, id));
     res.send({message:`Documento: ${id} eliminado`})
--- src/apifirebase/src/routes/users/validaciones.js
+++ src/apifirebase/src/routes/users/validaciones.js
@@ -3,19 +3,29 @@ function isEmail(email){
     return ExpRegularEmail.test(email)
 }
 
-export default function validate(nombre, edad, email){
+function isPassword(password){
+    const ExpRegularPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$/
+    return ExpRegularPassword.test(password)
+}
 
-    nombre= typeof nombre === 'string' && nombre != '' && nombre.length >= 4 ? true : false 
+export default function validate(username, nombre, edad, email, password){
+
+    username= typeof username === 'string' && username != '' && username.length >= 4 ? true : false 
+    nombre= typeof nombre === 'string' && nombre != '' && nombre.length >= 8 ? true : false 
+    password= typeof password === 'string' && password != '' && password.length >= 8  && isPassword(password)? true : false 
     edad = typeof edad === 'number' && edad != ''  ? true : false
     email = typeof email === 'string' && email != '' && isEmail(email) ? true : false
 
-    if (nombre && edad && email) {
+    if (nombre && edad && email && password && username) {
         return { validation  : true }
     }else{
         let msgValidado = 'Campo con validación correcta'
         let msg= []
+        let msgUsername = {
+            username: username ? msgValidado : 'Campo tipo String requerido, mínimo 4 letras'
+        }
         let msgNombre = {
-            nombre: nombre ? msgValidado : 'Campo tipo String requerido, mínimo 4 letras'
+            nombre: nombre ? msgValidado : 'Campo tipo String requerido, mínimo 8 letras'
         }
         let msgEdad = {
             edad: edad ? msgValidado : 'Campo tipo Number requerido'
@@ -23,9 +33,14 @@ export default function validate(nombre, edad, email){
         let msgEmail = {
             email: email ? msgValidado : 'Campo tipo String requerido'
         }
+        let msgPassword = {
+            password: password ? msgValidado : 'Campo tipo String requerido mínimo 8 caracteres, máximo 15 caracteres al menos una letra mayúscula, al menos un número, al menos un carácter especial [$, @, ?, %, &, !] '
+        }
+        !username ? msg.push(msgUsername): null 
         !nombre ? msg.push(msgNombre): null 
         !email ?  msg.push(msgEmail): null
         !edad ? msg.push(msgEdad) : null
+        !password ? msg.push(msgPassword) : null
         return {validation : false, msg}
     }
 }
